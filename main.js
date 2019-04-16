dbe775d7d7f4c45fb6c2d979b8e8b09fe3cebf30
ckrueger6 <kitcatherinek@gmail.com>
2019-04-15 19:10:59 -0500

    here are some method structures that can be stolen for box drawing

--- main.js
+++ main.js
@@ -17,11 +17,38 @@ const architectureListItemTemplate = Handlebars.compile($("#ali-template").html(
 function addArchitecture(architecture) {
     const json = parser.parse(architecture);
     renderNewArchitectureListItem(architecture, json);
-
-    // Do other things for adding architectures
+	createApplicationBox(json);
+	json.application.components.Component.forEach(function(element) {
+		createComponentBox(element);
+	});
+	json.application.newIntents.Intent.forEach(function(element){
+		createIntentBox(element);
+	});
     console.log(json);
 }
 
 function renderNewArchitectureListItem(xml, json) {
     $("#architecture-list").append(architectureListItemTemplate({ plaintext: xml, object: JSON.stringify(json) }));
+}
+
+function createApplicationBox(thing){
+	console.log("this is the beginning of the app box");
+	console.log(thing.application.packageName);
+	console.log("this is the end of the app box");
+}
+
+function createComponentBox(thing){
+	console.log("this is a component");
+	console.log(thing.type);
+	console.log(thing.name);
+	console.log(thing.IntentFilter.filter);
+	console.log("this is the end of a component");
+}
+
+function createIntentBox(thing){
+	console.log("this is an intent");
+	console.log(thing.calledAt);
+	console.log(thing.sender);
+	console.log(thing.consumerMethod);
+	console.log("this is the end of an intent")
 }
