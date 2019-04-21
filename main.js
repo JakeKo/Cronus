7f44cbcd50b7de8904c1ac8d318f95dfa3206a94
ckrueger6 <kitcatherinek@gmail.com>
2019-04-21 13:45:58 -0500

    just destroy all the canvas work i guess

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const aliTemplate = Handlebars.compile($("#ali-template").html());
-let index = -1;
+let index = 0;
 let manifestUploaded = false;
 let vulnerabilitiesUploaded = false;
+let row=0;
 
 // Run this function on page load
 (function () {
@@ -10,7 +11,7 @@ let vulnerabilitiesUploaded = false;
 
 // This function is responsible for instantiating a new architecture list item and yea
 function createArchitectureListItem() {
-    index++;
+    
 
     // Render a new architecture list item using the template
     $("#architecture-list").append(aliTemplate({ index: index }));
@@ -18,6 +19,7 @@ function createArchitectureListItem() {
     // Since new input elements were created, we need to bind event handlers
     $(`#ali-manifest-upload-${index}`).on("input", handleManifestUpload(index));
     $(`#ali-vulnerabilities-upload-${index}`).on("input", handleVulnerabilitiesUpload(index));
+	index++;
 }
 
 // Since everything is index-based, we need to pass in an index parameter
@@ -26,10 +28,14 @@ function handleManifestUpload(index) {
     return function handler(event) {
         const fileReader = new FileReader();
 
-        fileReader.addEventListener("loadend", function (event) {
-            $(`#ali-manifest-raw-${index}`).val(event.target.result);
-            console.log(parser.parse(event.target.result));
+		fileReader.addEventListener("loadend", function (event) {
+            addArchitecture(event.target.result, index);
         });
+		
+        // fileReader.addEventListener("loadend", function (event) {
+            // $(`#ali-manifest-raw-${index}`).val(event.target.result);
+            // console.log(parser.parse(event.target.result));
+        // });
 
         fileReader.readAsText(event.target.files[0]);
         $(`#ali-manifest-upload-label-${index}`).hide();
@@ -45,82 +51,36 @@ function handleManifestUpload(index) {
     }
 }
 
-function addArchitecture(architecture) {
-	var c = document.getElementById("myCanvas");
-	var ctx = c.getContext("2d");
-	ctx.canvas.width  = window.innerWidth*.5;
-	ctx.canvas.height = window.innerHeight*.5;
+function addArchitecture(architecture,index) {
+	var id = `ali-manifest-raw-${index}`;
     const json = parser.parse(architecture);
-    renderNewArchitectureListItem(architecture, json);
-	createApplicationBox(json,ctx);
-	x=-1;
+	createApplication(json, id);
+	document.getElementById(id).value+="\n";
+	x=1;
 	json.application.components.Component.forEach(function(element) {
-		createComponentBox(element,x++,ctx);
+		createComponent(element,x++,id);
 	});
-	y=-1;
+	document.getElementById(id).value+="\n";
+	y=1;
+	row=0;
 	json.application.newIntents.Intent.forEach(function(element){
-		createIntentBox(element,y++,ctx);
+		createIntent(element,y++,id);
 	});
     console.log(json);
 }
 
-function renderNewArchitectureListItem(xml, json) {
-    $("#architecture-list").append(architectureListItemTemplate({ plaintext: xml, object: JSON.stringify(json) }));
+
+function createApplication(app, id){
+	document.getElementById(id).value = "Application name: " + app.application.packageName + "\n";
 }
 
-function createApplicationBox(thing,ctx){
-
-	// Red rectangle
-	ctx.beginPath();
-	ctx.lineWidth = "6";
-	ctx.strokeStyle = "red";
-	ctx.rect(5, 5, ctx.canvas.width-10, ctx.canvas.height-10); 
-	ctx.stroke();
-	
-	ctx.fillStyle = "red";
-    ctx.font = "10pt sans-serif";
-    ctx.fillText(thing.application.packageName, 10, 30);
+function createComponent(comp,x, id){
+	document.getElementById(id).value+= "Component " +`${x}`+":\nType: " + comp.type + "\nName: " + comp.name + "\nFilter: " + comp.IntentFilter.filter + "\n\n";
 }
 
-function createComponentBox(thing,x,ctx){
-	// Green rectangle
-	ctx.beginPath();
-	ctx.lineWidth = "4";
-	ctx.strokeStyle = "green";
-	if(x<ctx.canvas.width){
-		ctx.rect(50+x*110, 100, 100, 40);
-		ctx.stroke();
-	}else{
-		y=ctx%x;
-		z=ctx/x;
-		ctx.rect(50+y*110, 100+z*50, 100, 40);
-		ctx.stroke();
-	}
-	console.log("this is a component");
-	console.log(thing.type);
-	console.log(thing.name);
-	console.log(thing.IntentFilter.filter);
-	console.log("this is the end of a component");
+function createIntent(intent,x,id){
+	document.getElementById(id).value+= "Intent " + `${x}` +":\nCalled at " + intent.calledAt + "\nSent by: " + intent.sender + "\nConsumed by: "+ intent.consumerMethod + "\n\n";
 }
-
-function createIntentBox(thing,x,ctx){
-	ctx.beginPath();
-	ctx.lineWidth = "4";
-	ctx.strokeStyle = "blue";
-	if(x<ctx.canvas.width){
-		ctx.rect(70+x*110, .5*ctx.canvas.height, 100, 40);
-		ctx.stroke();
-	}else{
-		y=ctx%x;
-		z=ctx/x;
-		ctx.rect(70+y*110, z*50+.5*ctx.canvas.height, 100, 40);
-		ctx.stroke();
-	}
-	console.log("this is an intent");
-	console.log(thing.calledAt);
-	console.log(thing.sender);
-	console.log(thing.consumerMethod);
-	console.log("this is the end of an intent");
 // Since everything is index-based, we need to pass in an index parameter
 // So we return an event handler (which can only accept a single event parameter) with the index
 function handleVulnerabilitiesUpload(index) {
