ce0a647bf431feddde052e293e74dc0fe2ef0a02
Jake Koperski <wijakoperski@gmail.com>
2019-04-21 20:29:38 -0500

    Added naiive diff checking on vulnerabilities

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const aliTemplate = Handlebars.compile($("#ali-template").html());
-let index = -1;
+const architectureList = [];
 let manifestUploaded = false;
 let vulnerabilitiesUploaded = false;
 
@@ -10,7 +10,9 @@ let vulnerabilitiesUploaded = false;
 
 // This function is responsible for instantiating a new architecture list item and yea
 function createArchitectureListItem() {
-    index++;
+    // Push an empty object to the list so we can update it later with the manifest and vulnerabilities files
+    architectureList.push({});
+    const index = architectureList.length - 1;
 
     // Render a new architecture list item using the template
     $("#architecture-list").append(aliTemplate({ index: index }));
@@ -27,8 +29,11 @@ function handleManifestUpload(index) {
         const fileReader = new FileReader();
 
         fileReader.addEventListener("loadend", function (event) {
-            $(`#ali-manifest-raw-${index}`).val(event.target.result);
-            console.log(parser.parse(event.target.result));
+            const manifest = event.target.result;
+            $(`#ali-manifest-raw-${index}`).val(manifest);
+
+            // Update the architecture list with the newly uploaded content
+            architectureList[index].manifest = parser.parse(manifest);
         });
 
         fileReader.readAsText(event.target.files[0]);
@@ -52,8 +57,12 @@ function handleVulnerabilitiesUpload(index) {
         const fileReader = new FileReader();
 
         fileReader.addEventListener("loadend", function (event) {
-            $(`#ali-vulnerabilities-raw-${index}`).val(event.target.result);
-            console.log(parser.parse(event.target.result));
+            const vulnerabilities = event.target.result;
+            $(`#ali-vulnerabilities-raw-${index}`).val(vulnerabilities);
+
+            architectureList[index].vulnerabilities = parser.parse(vulnerabilities);
+            // console.log(architectureList[index].vulnerabilities);
+            displayDiff(calculateDiff(index - 1, index));
         });
 
         fileReader.readAsText(event.target.files[0]);
@@ -68,4 +77,31 @@ function handleVulnerabilitiesUpload(index) {
             vulnerabilitiesUploaded = true;
         }
     }
+}
+
+function calculateDiff(firstIndex, secondIndex) {
+    const first = architectureList[firstIndex];
+    const second = architectureList[secondIndex];
+    return DeepDiff.diff(
+        first === undefined ? {} : first.vulnerabilities.analysisReport.vulnerabilities.vulnerability,
+        second.vulnerabilities.analysisReport.vulnerabilities.vulnerability
+    );
+}
+
+function displayDiff(diff) {
+    console.log(diff);
+    const vulnerabilitiesAdded = [];
+    const vulnerabilitiesRemoved = [];
+    
+    diff.forEach(diffInstance => {
+        if (diffInstance.kind === "E") {
+            vulnerabilitiesAdded.push(...diffInstance.rhs.map(item => item.type));
+        } else if (diffInstance.item.kind === "D") {
+            vulnerabilitiesRemoved.push(diffInstance.item.lhs.type);
+        } else if (diffInstance.item.kind === "N") {
+            vulnerabilitiesAdded.push(diffInstance.item.rhs.type);
+        }
+    });
+
+    console.log({ vulnerabilitiesAdded, vulnerabilitiesRemoved });
 }
